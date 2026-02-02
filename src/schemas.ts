cf08333c4afe8033083f38617da17427446b62fc
Sergio Casado <sercassan@gmail.com>
2026-02-02 18:43:06 +0100

    feat: add nullable schema

--- src/schemas.ts
+++ src/schemas.ts
@@ -14,6 +14,7 @@ import { TyafeObject } from "./structural/object";
 import { TyafeRecord } from "./structural/record";
 import { TyafeTuple } from "./structural/tuple";
 import type { LiteralParts } from "./types";
+import { TyafeNullable } from "./utility/nullable";
 import { TyafeOptional } from "./utility/optional";
 
 export function string(error?: string): TyafeString {
@@ -91,6 +92,12 @@ export function optional<T extends TyafeBase<any, any>>(
   schema: T,
 ): TyafeOptional<T> {
   return new TyafeOptional(schema);
+}
+
+export function nullable<T extends TyafeBase<any, any>>(
+  schema: T,
+): TyafeNullable<T> {
+  return new TyafeNullable(schema);
 }
 
 export { undefined_s as undefined, null_s as null };
