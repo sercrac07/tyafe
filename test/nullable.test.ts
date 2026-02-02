501117b25af0a3170520df04b9514aa860f6842f
Sergio Casado <sercassan@gmail.com>
2026-02-02 18:42:53 +0100

    tests: add tests for nullable schema

---
+++ test/nullable.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from "vitest";
+import { type T, t } from "../src";
+import type { Expect } from "./utils";
+
+const _schema = t.nullable(t.string());
+const _input: Expect<T.Input<typeof _schema>, string | null> = null as any;
+const _output: Expect<T.Output<typeof _schema>, string | null> = null as any;
+
+describe("nullable schema", () => {
+  it("should be defined", () => {
+    expect(t.nullable).toBeDefined();
+  });
+
+  it("should parse", () => {
+    expect(t.nullable(t.string()).parse("john")).toBe("john");
+    expect(t.nullable(t.string()).parse(null)).toBe(null);
+  });
+
+  it("should not parse", () => {
+    expect(() => t.nullable(t.string()).parse()).toThrow();
+    expect(() => t.nullable(t.string()).parse(1)).toThrow();
+    expect(() => t.nullable(t.string()).parse(1n)).toThrow();
+    expect(() => t.nullable(t.string()).parse(true)).toThrow();
+    expect(() => t.nullable(t.string()).parse(undefined)).toThrow();
+  });
+});
