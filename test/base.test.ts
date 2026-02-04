f8f165ca2fef3db7d8a89d16b0967996cc9af9ed
Sergio Casado <sercassan@gmail.com>
2026-02-04 14:14:19 +0100

    test(base): add async test cases

--- test/base.test.ts
+++ test/base.test.ts
@@ -1,18 +1,27 @@
 import { describe, expect, it } from "vitest";
-import { t } from "../src";
+import { ERROR_CODES, t } from "../src";
 
 describe("base functionality", () => {
+  it("should parse", () => {
+    expect(t.string().parse("john")).toBe("john");
+    expect(() => t.string().parse(12)).toThrow();
+  });
+
+  it("should parse async", async () => {
+    await expect(t.string().parseAsync("john")).resolves.toBe("john");
+    await expect(t.string().parseAsync(12)).rejects.toThrow();
+  });
+
   it("should safe parse", () => {
     expect(t.string().safeParse("john")).toEqual({
       success: true,
       data: "john",
     });
-
     expect(t.string().safeParse(12)).toEqual({
       success: false,
       issues: [
         {
-          code: "invalid_type",
+          code: ERROR_CODES.CORE.INVALID_TYPE,
           error: "Invalid input type: string was expected",
           path: [],
         },
@@ -20,7 +29,24 @@ describe("base functionality", () => {
     });
   });
 
-  it("should handle default values", () => {
+  it("should safe parse async", async () => {
+    await expect(t.string().safeParseAsync("john")).resolves.toEqual({
+      success: true,
+      data: "john",
+    });
+    await expect(t.string().safeParseAsync(12)).resolves.toEqual({
+      success: false,
+      issues: [
+        {
+          code: ERROR_CODES.CORE.INVALID_TYPE,
+          error: "Invalid input type: string was expected",
+          path: [],
+        },
+      ],
+    });
+  });
+
+  it("should handle default values", async () => {
     expect(t.string().default("john").parse()).toBe("john");
     expect(
       t
@@ -28,6 +54,19 @@ describe("base functionality", () => {
         .default(() => "john")
         .parse(),
     ).toBe("john");
+    expect(() =>
+      t
+        .string()
+        .default(async () => "john")
+        .parse(),
+    ).toThrow();
+    await expect(
+      t
+        .string()
+        .default(async () => "john")
+        .parseAsync(),
+    ).resolves.toBe("john");
+
     expect(t.string().fallback("john").parse()).toBe("john");
     expect(
       t
@@ -35,9 +74,21 @@ describe("base functionality", () => {
         .fallback(() => "john")
         .parse(),
     ).toBe("john");
+    expect(() =>
+      t
+        .string()
+        .fallback(async () => "john")
+        .parse(),
+    ).toThrow();
+    await expect(
+      t
+        .string()
+        .fallback(async () => "john")
+        .parseAsync(),
+    ).resolves.toBe("john");
   });
 
-  it("should validate", () => {
+  it("should validate", async () => {
     expect(
       t
         .string()
@@ -51,18 +102,46 @@ describe("base functionality", () => {
         .validate((value) => (value !== "john" ? null : "Invalid"))
         .parse("john"),
     ).toThrow();
+
+    expect(() =>
+      t
+        .string()
+        .validate(async (value) => (value !== "doe" ? null : "Invalid"))
+        .parse("john"),
+    ).toThrow();
+
+    await expect(
+      t
+        .string()
+        .validate(async (value) => (value !== "doe" ? null : "Invalid"))
+        .parseAsync("john"),
+    ).resolves.toBe("john");
   });
 
-  it("should process", () => {
+  it("should process", async () => {
     expect(
       t
         .string()
         .process((value) => value.toUpperCase())
         .parse("john"),
     ).toBe("JOHN");
+
+    expect(() =>
+      t
+        .string()
+        .process(async (value) => value.toUpperCase())
+        .parse("john"),
+    ).toThrow();
+
+    await expect(
+      t
+        .string()
+        .process(async (value) => value.toUpperCase())
+        .parseAsync("john"),
+    ).resolves.toBe("JOHN");
   });
 
-  it("should preprocess", () => {
+  it("should preprocess", async () => {
     expect(
       t
         .string()
@@ -70,11 +149,29 @@ describe("base functionality", () => {
         .preprocess<string>((value) => value.toUpperCase())
         .parse("john"),
     ).toBe("JOHN");
+
+    expect(() =>
+      t
+        .string()
+        .preprocess((value) => String(value))
+        .preprocess<string>(async (value) => value.toUpperCase())
+        .parse("john"),
+    ).toThrow;
+
+    await expect(
+      t
+        .string()
+        .preprocess((value) => String(value))
+        .preprocess<string>(async (value) => value.toUpperCase())
+        .parseAsync("john"),
+    ).resolves.toBe("JOHN");
   });
 
   it("should clone config", () => {
-    expect(t.string().min(4).clone().parse("john")).toBe("john");
+    const schema = t.string().email();
+    const newSchema = schema.clone().min(4);
 
-    expect(() => t.string().min(5).clone().parse("john")).toThrow();
+    expect(schema.parse("john@exam.ple")).toBe("john@exam.ple");
+    expect(() => newSchema.parse("john")).toThrow();
   });
 });
